577ed37497555c95b62dae18d102a35da1fcd0d8
Michael Zabka <zabka.michael@gmail.com>
2016-02-16 11:01:04 +0100

    Add quadratic sequence function using es6 generators with yield

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ES6 testing features
+
+## Quadratic sequence generator
+```sh
+node --harmony ./quadraticSequence.js {q} {n}
+```
---
+++ quadraticSequence.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+console.log('start');
+
+function *sequence(q, count) {
+	"use strict";
+	let array = [];
+	let a = yield;
+	for (let i = 0;i < count;i++) {
+		array.push(a);
+		a = yield a * q;
+	}
+	return array.join(',');
+}
+
+function start() {
+	"use strict";
+	let an, nextAn = 1;
+	const sequenceGenerator = sequence(process.argv[2], process.argv[3]);
+	do {
+		an = sequenceGenerator.next(nextAn);
+		nextAn = an.value || nextAn;
+		console.log('item', nextAn);
+	} while (!an.done);
+}
+
+start();
+
+console.log('end');
